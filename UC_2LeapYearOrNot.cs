84f5f3d6af932066622b7807b2c4cc0d1b615bbc
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-17 21:14:07 +0530

    [akarsh] added: Checking Leap Year Or Not

---
+++ UC_2LeapYearOrNot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5assigment_BasicCorePrograms
+{
+    class UC_2LeapYearOrNot
+    {
+        public void CheckLeapOrNot()
+        {
+            Console.WriteLine("Enter to Year of 4 digits to Check if it is Leap Year or Not: ");
+
+           
+            int year = Convert.ToInt32(Console.ReadLine()); 
+
+            if (( year % 4 == 0 && (year % 10 != 0)))
+
+                Console.WriteLine(year + " is a Leap Year");
+            else
+                Console.WriteLine(year + " is not a Leap Year");
+           
+        }
+    }
+}
+
