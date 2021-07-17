65cd820711489b364315efd9aab663fbb308d1df
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-17 23:18:24 +0530

    [akarsh] added: Check Alphabet Vowel Or Not

---
+++ UC_9VowelOrNot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Day5assigment_BasicCorePrograms
+{
+    class UC_9VowelOrNot
+    {
+        public Char Ch;
+
+        public void CheckGivenVowel()
+        {
+            Console.Write("Enter a is a vowel or not:\n");
+
+            Ch = Convert.ToChar(Console.ReadLine());
+            if (Ch == 'A' | Ch == 'E' | Ch == 'I' | Ch == 'O' | Ch == 'U' | Ch == 'a' | Ch == 'e' | Ch == 'i' | Ch == 'o' | Ch == 'u')
+            { 
+
+            Console.WriteLine(Ch + " is a Vowel");
+
+            }
+
+            else
+                {
+                    Console.WriteLine(Ch + " is a Consonant");
+                }
+        }
+
+    }
+}
