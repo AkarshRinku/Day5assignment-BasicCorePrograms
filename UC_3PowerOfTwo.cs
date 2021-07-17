a8b0bdbb0737a6b1ce0cd5b89dfc87b1e7634000
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-17 21:49:58 +0530

    [akarsh] added: Checking Power Of Two

---
+++ UC_3PowerOfTwo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5assigment_BasicCorePrograms
+{
+    class UC_3PowerOfTwo
+    {
+        public int num = 0;
+        public void CalculatePowerOf2()
+        {
+
+            Console.WriteLine("Enter a number:");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int result = num * 2; 
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
+
+    
+
