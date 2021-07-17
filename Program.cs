127f217fa34d8c0bab1039d8ba3d655537ed681f
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-17 21:08:02 +0530

    [akarsh] added: Welcome To Basic Core Programs.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Day5assigment_BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC_1FlipCoinHeadsAndTails HeadTail1 = new UC_1FlipCoinHeadsAndTails();
+            //HeadTail1.CheckHeadsOrTailsPercentage();
+
+            //UC_2LeapYearOrNot LeapYear1 = new UC_2LeapYearOrNot();
+            //LeapYear1.CheckLeapOrNot();
+
+            //UC_3PowerOfTwo TwoSquare = new UC_3PowerOfTwo();
+            //TwoSquare.CalculatePowerOf2();
+
+            //UC_4HarmonicNumber HarNum = new UC_4HarmonicNumber();
+            //HarNum.CalculateHarmonicSeries();
+
+            //UC_5PrimeFactor primefactor = new UC_5PrimeFactor();
+            //primefactor.Main();
+
+            //UC_6QuotientAndRemainder quotientremainder = new UC_6QuotientAndRemainder();
+            //quotientremainder.Division();
+
+            //UC_7SwapTwoNumbers swapTwonumbers = new UC_7SwapTwoNumbers();
+            //swapTwonumbers.Swap2Number();
+
+            //UC_8EvenOrOdd evenodd = new UC_8EvenOrOdd();
+            //evenodd.CheckEvenOrdd();
+
+            //UC_9VowelOrNot vowelornot = new UC_9VowelOrNot();
+            //vowelornot.CheckGivenVowel();
+
+            //UC_10LargestAmongThreeNos largestamongthree = new UC_10LargestAmongThreeNos();
+            //largestamongthree.checkLargestNumber();
+            //Console.ReadKey();
+        }
+    }
+}
+
