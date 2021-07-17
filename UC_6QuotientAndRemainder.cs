8ade8bf5a83d47371cb065ce8ee8789227142233
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-17 23:11:20 +0530

    [akarsh] added: Compute Quotient And Remainder

---
+++ UC_6QuotientAndRemainder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5assigment_BasicCorePrograms
+{
+    class UC_6QuotientAndRemainder
+    {
+        public void Division()
+        {
+            Console.WriteLine("Enter the Dividend : ");
+            int Dividend = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Divisor : ");
+            int Divisor = Convert.ToInt32(Console.ReadLine());
+
+
+            int quotient = Dividend / Divisor;
+            int remainder = Dividend % Divisor;
+
+            Console.WriteLine("Dividend: " + Dividend + " Divisor: " + Divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}
