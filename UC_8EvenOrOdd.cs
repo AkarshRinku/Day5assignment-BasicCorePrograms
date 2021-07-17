cbb4d60a5315a795c627e65ee7ae93c65ff6041b
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-17 23:15:48 +0530

    [akarsh] added: Check Number Even Or Odd

---
+++ UC_8EvenOrOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5assigment_BasicCorePrograms
+{
+    class UC_8EvenOrOdd
+    {
+        public int Number;
+        public int Remainder;
+        public void CheckEvenOrdd()
+        {
+
+            Console.Write("Enter Number to Check whether Number is Even or Odd :\n");
+            
+            Number = Convert.ToInt32(Console.ReadLine());
+            Remainder = Number % 2;
+
+            if (Remainder == 0)
+                Console.WriteLine("{0} is an Even Number.", Number);
+            else
+                Console.WriteLine("{0} is an Odd Number.", Number);
+        }
+    }
+}
